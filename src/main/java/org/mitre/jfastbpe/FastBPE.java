59ece2f607104bfa660532091d2f185910daa793
Elijah Rippeth <erippeth@mitre.org>
2020-04-28 09:53:36 -0400

    add additional constructors.

--- src/main/java/org/mitre/jfastbpe/FastBPE.java
+++ src/main/java/org/mitre/jfastbpe/FastBPE.java
@@ -8,8 +8,16 @@ import java.io.IOException;
 public class FastBPE {
     final private FastBPEAnalyzer analyzer;
 
-    public FastBPE(final File f) throws IOException {
-        analyzer = new FastBPEAnalyzer(f);
+    public FastBPE(final File codesFile) throws IOException {
+        analyzer = new FastBPEAnalyzer(codesFile);
+    }
+
+    public FastBPE(final File codesFile, final File vocabFile) throws IOException {
+        analyzer = new FastBPEAnalyzer(codesFile, vocabFile, 4);
+    }
+
+    public FastBPE(final File codesFile, final File vocabFile, int numThreads) throws IOException {
+        analyzer = new FastBPEAnalyzer(codesFile, vocabFile, numThreads);
     }
 
     public String applyBPE(final String sent) throws InterruptedException {
